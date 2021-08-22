c68cb9204f99e691e20a6563d3a81a0ca42254f8
vaibhav-if <vaibhavagr711@gmail.com>
2021-08-22 20:04:45 +0530

    Add and Refactor Button Component

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.svg";
-import Button from "@material-ui/core/Button";
+import ActionButton from "../components/ActionButton";
 
-const Header = () => {
-  const buttonName = "Login"; // variable based on the access token value
+export default function Header() {
   return (
     <div className="header">
-      <img id='logo' src={Logo} alt="Book Movie App Logo" />
-      <Button className="nav-btn" variant="contained">
-        {buttonName}
-      </Button>
+      <img id="logo" src={Logo} alt="Book Movie App Logo" />
+      <ActionButton
+        className="nav-btn"
+        variant="contained"
+        color="default"
+        buttonName="Login"
+      />
       {/* should always be displayed in the header when a user clicks on a released movie */}
-      {/* <Button className="nav-btn" variant="contained" color="primary">
-        Book Show
-      </Button> */}
+      <ActionButton
+        className="nav-btn"
+        variant="contained"
+        color="primary"
+        buttonName="Book Show"
+      />
     </div>
   );
-};
-
-export default Header;
+}
--- BookMovieApp/src/index.js
+++ BookMovieApp/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Header from './common/header/Header';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "typeface-roboto";
+import registerServiceWorker from "./registerServiceWorker";
+import Header from "./common/header/Header";
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById("root"));
 registerServiceWorker();

17bfd9115ed7425ef4c8b5f677926806e93246ae
vaibhav-if <vaibhavagr711@gmail.com>
2021-08-23 16:22:12 +0530

    Add Form Components to Filter Card

--- BookMovieApp/src/common/components/FilterCard.js
+++ BookMovieApp/src/common/components/FilterCard.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-// import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 240,
     maxWidth: 240,
-    margin: theme.spacing.unit,
   },
   title: {
     fontSize: 14,
     color: theme.palette.primary.light,
+    margin: theme.spacing.unit,
   },
-  //   pos: {
-  //     marginBottom: 12,
-  //   },
+  form: {
+    margin: theme.spacing.unit,
+    minWidth: 192,
+  },
+  apply: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 export default function FilterCard() {
@@ -30,8 +39,48 @@ export default function FilterCard() {
         <Typography className={classes.title} gutterBottom>
           FIND MOVIES BY:
         </Typography>
+        <FormControl className={classes.form}>
+          <InputLabel id="movie-name">Movie Name</InputLabel>
+          <Input labelId="movie-name" />
+        </FormControl>
+        <FormControl className={classes.form}>
+          <InputLabel id="genre">Genre</InputLabel>
+          <Select labelId="genre">
+            <MenuItem></MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.form}>
+          <InputLabel id="artist">Artist</InputLabel>
+          <Select labelId="artist">
+            <MenuItem></MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.form}>
+          <TextField
+            id="relese-date-start"
+            label="Release Date Start"
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </FormControl>
+        <FormControl className={classes.form}>
+          <TextField
+            id="relese-date-end"
+            label="Release Date End"
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </FormControl>
+        <FormControl className={classes.form}>
+          <Button className={classes.apply} variant="contained" color="primary">
+            APPLY
+          </Button>
+        </FormControl>
       </CardContent>
-      <CardActions></CardActions>
     </Card>
   );
 }

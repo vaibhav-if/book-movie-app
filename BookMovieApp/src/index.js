f35040426ffce00f763c5ff9afc41c5492d5d966
vaibhav-if <vaibhavagr711@gmail.com>
2021-08-26 19:22:27 +0530

    Add Controller.js

--- BookMovieApp/src/index.js
+++ BookMovieApp/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import "typeface-roboto";
 import registerServiceWorker from "./registerServiceWorker";
 import Home from "./screens/home/Home";
+// import Controller from "./screens/Controller"
 
 ReactDOM.render(<Home />, document.getElementById("root"));
 registerServiceWorker();
